ab63661f9b56b060e24874700b8503d6976d16ab
Zonglei Dong <dongzonglei@apache.org>
2022-09-22 17:08:37 +0800

    add hyper_shutdown demo.

---
+++ hyper_shutdown/src/main.rs
@@ -0,0 +1,28 @@
+use hyper::Server;
+use hyper::service::{make_service_fn, service_fn};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr = ([127, 0, 0, 1], 3000).into();
+
+    let service = make_service_fn(|_| async { Ok::<_, hyper::Error>(service_fn(echo)) });
+
+    let server = Server::bind(&addr).serve(service);
+
+    // And now add a graceful shutdown signal...
+    let graceful = server.with_graceful_shutdown(shutdown_signal());
+
+    // Run this server for... forever!
+    if let Err(e) = graceful.await {
+        eprintln!("server error: {}", e);
+    }
+
+    Ok(())
+}
+
+async fn shutdown_signal() {
+    // Wait for the CTRL+C signal
+    tokio::signal::ctrl_c()
+        .await
+        .expect("failed to install CTRL+C signal handler");
+}
