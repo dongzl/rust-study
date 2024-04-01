eadcc52d143c3d536c3f4f0371124cae56850f05
Zonglei Dong <dongzonglei@apache.org>
2024-04-01 16:11:51 +0800

    rust study

--- rust_in_action/ch2/src/main.rs
+++ rust_in_action/ch2/src/main.rs
@@ -81,18 +81,53 @@
 //     println!("{}", count);
 // }
 
+// fn main() {
+//     //let needle = 42;
+//     let haystack = [1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862];
+//
+//     for item in &haystack {
+//         let result = match item {
+//             42 | 132 => "hit!",
+//             _ => "miss",
+//         };
+//
+//         if result == "hit!" {
+//             println!("{}: {}", item, result);
+//         }
+//     }
+// }
+
+// fn main() {
+//     let a = 42;
+//     let r = &42;
+//     let b = a + *r;
+//     println!("a + a = {}", b);
+// }
+
+// fn add_with_lifetimes<'a, 'b>(i: &'a i32, j: &'b i32) -> i32 {
+//     *i + *j
+// }
+
+// fn main() {
+//     let a = 10;
+//     let b = 20;
+//     let res = add_with_lifetimes(&a, &b);
+//     println!("{}", res);
+// }
+
+use std::ops::Add;
+use std::time::Duration;
+
+fn add<T: Add<Output = T>>(i: T, j: T) -> T {
+    i + j
+}
+
 fn main() {
-    //let needle = 42;
-    let haystack = [1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862];
+    let floats = add(1.2, 3.4);
+    let ints = add(10, 20);
+    let durations = add(Duration::new(5, 0), Duration::new(10, 0));
 
-    for item in &haystack {
-        let result = match item {
-            42 | 132 => "hit!",
-            _ => "miss",
-        };
-
-        if result == "hit!" {
-            println!("{}: {}", item, result);
-        }
-    }
+    println!("{}", floats);
+    println!("{}", ints);
+    println!("{:?}", durations);
 }
