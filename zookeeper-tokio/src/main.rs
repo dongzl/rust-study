6963eac66d5bcbc5ff7b14e7378d11d3f694cfb5
Zonglei Dong <dongzonglei@apache.org>
2024-05-08 16:05:57 +0800

    rust study

--- zookeeper-tokio/src/main.rs
+++ zookeeper-tokio/src/main.rs
@@ -1,77 +1,69 @@
-use std::io::Write;
+use std::rc::Rc;
 use tokio::fs::File;
 use tokio::io::AsyncWriteExt;
 use tokio_zookeeper::*;
-//use futures::prelude::*;
 
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
-
-    // let (zk, default_watcher) = ZooKeeper::connect(&"127.0.0.1:2181".parse().unwrap())
-    //     .await
-    //     .unwrap();
-    // let stat = zk.watch().exists("/example").await.unwrap();
-    // assert_eq!(stat, None);
-    // let path = zk
-    //     .create(
-    //         "/example",
-    //         &b"Hello world"[..],
-    //         Acl::open_unsafe(),
-    //         CreateMode::Persistent,
-    //     )
-    //     .await
-    //     .unwrap();
-    // assert_eq!(path.as_deref(), Ok("/example"));
-
-    // let children = zk.get_children("/governance_ds/metadata/sys/schemas/sys/tables").await.unwrap();
-    //
-    // //println!("children {:?}", children);
-    //
-    // for child in children.unwrap().iter() {
-    //     println!("Child {:?}", child);
-    //     let path = format!("{}{}{}", "/governance_ds/metadata/sys/schemas/sys/tables/", child, "/versions/0");
-    //     println!("Path {}", path);
-    //     let data = zk.get_data(&path).await.unwrap();
-    //     let data = data.unwrap();
-    //     //println!("Data {:?}", data.0);
-    //     let s = String::from_utf8(data.0).expect("Found invalid UTF-8");
-    //     //println!("{}", s);
-    //
-    //
-    //     let mut file = File::create(format!("{}{}{}", "sys/", child, ".yaml")).await.unwrap();
-    //     file.write(s.as_bytes()).await.unwrap();
-    // }
-
-    export_sys().await;
-    export_performance_schema().await;
-    export_mysql().await;
-    export_information_schema().await;
+    let (zk, _) = ZooKeeper::connect(&"127.0.0.1:2181".parse().unwrap())
+        .await
+        .unwrap();
+    let zk = Rc::new(zk);
+    export(zk.clone(), "/governance_ds/metadata/sys/schemas/sys/tables", "database/sys/").await;
+    export(zk.clone(), "/governance_ds/metadata/performance_schema/schemas/performance_schema/tables", "database/performance_schema/").await;
+    export(zk.clone(), "/governance_ds/metadata/mysql/schemas/mysql/tables", "database/mysql/").await;
+    export(zk.clone(), "/governance_ds/metadata/information_schema/schemas/information_schema/tables", "database/information_schema/").await;
 }
 
-async fn export_sys() {
-    let (zk, default_watcher) = ZooKeeper::connect(&"127.0.0.1:2181".parse().unwrap())
-        .await
-        .unwrap();
+async fn export(zk: Rc<ZooKeeper>, path: &str, export: &str) {
     let children = zk
-        .get_children("/governance_ds/metadata/sys/schemas/sys/tables")
+        .get_children(path)
         .await
         .unwrap();
 
-    //println!("children {:?}", children);
+    // println!("children {:?}", children);
 
     for child in children.unwrap().iter() {
         println!("Child {:?}", child);
         let path = format!(
-            "{}{}{}",
-            "/governance_ds/metadata/sys/schemas/sys/tables/", child, "/versions/0"
+            "{}{}{}{}",
+            path, "/", child, "/versions/0"
         );
         println!("Path {}", path);
         let data = zk.get_data(&path).await.unwrap();
         let data = data.unwrap();
-        //println!("Data {:?}", data.0);
+        // println!("Data {:?}", data.0);
         let s = String::from_utf8(data.0).expect("Found invalid UTF-8");
-        //println!("{}", s);
+        // println!("{}", s);
+
+        let mut file = File::create(format!("{}{}{}", export, child, ".yaml"))
+            .await
+            .unwrap();
+        file.write(s.as_bytes()).await.unwrap();
+    }
+}
+
+async fn export_sys(zk: Rc<ZooKeeper>, path: &str) {
+    let children = zk
+        .get_children(path)
+        .await
+        .unwrap();
+
+    // println!("children {:?}", children);
+
+    for child in children.unwrap().iter() {
+        println!("Child {:?}", child);
+        let path = format!(
+            "{}{}{}{}",
+            path, "/", child, "/versions/0"
+        );
+        println!("Path {}", path);
+        let data = zk.get_data(&path).await.unwrap();
+        let data = data.unwrap();
+        // println!("Data {:?}", data.0);
+        let s = String::from_utf8(data.0).expect("Found invalid UTF-8");
+        // println!("{}", s);
 
         let mut file = File::create(format!("{}{}{}", "database/sys/", child, ".yaml"))
             .await
@@ -80,13 +72,10 @@ async fn export_sys() {
     }
 }
 
-async fn export_performance_schema() {
-    let (zk, default_watcher) = ZooKeeper::connect(&"127.0.0.1:2181".parse().unwrap())
-        .await
-        .unwrap();
+async fn export_performance_schema(zk: Rc<ZooKeeper>, path: &str) {
     let children = zk
         .get_children(
-            "/governance_ds/metadata/performance_schema/schemas/performance_schema/tables",
+            path,
         )
         .await
         .unwrap();
@@ -96,8 +85,9 @@ async fn export_performance_schema() {
     for child in children.unwrap().iter() {
         println!("Child {:?}", child);
         let path = format!(
-            "{}{}{}",
-            "/governance_ds/metadata/performance_schema/schemas/performance_schema/tables/",
+            "{}{}{}{}",
+            path,
+            "/",
             child,
             "/versions/0"
         );
@@ -106,7 +96,7 @@ async fn export_performance_schema() {
         let data = data.unwrap();
         //println!("Data {:?}", data.0);
         let s = String::from_utf8(data.0).expect("Found invalid UTF-8");
-        //println!("{}", s);
+        // println!("{}", s);
 
         let mut file = File::create(format!(
             "{}{}{}",
@@ -118,12 +108,9 @@ async fn export_performance_schema() {
     }
 }
 
-async fn export_mysql() {
-    let (zk, default_watcher) = ZooKeeper::connect(&"127.0.0.1:2181".parse().unwrap())
-        .await
-        .unwrap();
+async fn export_mysql(zk: Rc<ZooKeeper>, path: &str) {
     let children = zk
-        .get_children("/governance_ds/metadata/mysql/schemas/mysql/tables")
+        .get_children(path)
         .await
         .unwrap();
 
@@ -132,8 +119,8 @@ async fn export_mysql() {
     for child in children.unwrap().iter() {
         println!("Child {:?}", child);
         let path = format!(
-            "{}{}{}",
-            "/governance_ds/metadata/mysql/schemas/mysql/tables/", child, "/versions/0"
+            "{}{}{}{}",
+            path, "/", child, "/versions/0"
         );
         println!("Path {}", path);
         let data = zk.get_data(&path).await.unwrap();
@@ -149,13 +136,10 @@ async fn export_mysql() {
     }
 }
 
-async fn export_information_schema() {
-    let (zk, default_watcher) = ZooKeeper::connect(&"127.0.0.1:2181".parse().unwrap())
-        .await
-        .unwrap();
+async fn export_information_schema(zk: Rc<ZooKeeper>, path: &str) {
     let children = zk
         .get_children(
-            "/governance_ds/metadata/information_schema/schemas/information_schema/tables",
+            path,
         )
         .await
         .unwrap();
@@ -165,8 +149,9 @@ async fn export_information_schema() {
     for child in children.unwrap().iter() {
         println!("Child {:?}", child);
         let path = format!(
-            "{}{}{}",
-            "/governance_ds/metadata/information_schema/schemas/information_schema/tables/",
+            "{}{}{}{}",
+            path,
+            "/",
             child,
             "/versions/0"
         );
