b2ad96233e3c4c6adc68db253dbfcf3fca28f478
Zonglei Dong <dongzonglei@apache.org>
2024-05-09 21:41:25 +0800

    rust study

---
+++ http_client/src/main.rs
@@ -0,0 +1,36 @@
+use std::collections::HashMap;
+use reqwest::header::HeaderMap;
+use serde_json::Value;
+
+async fn get(pin: &str) -> Result<HashMap<String, Value>, reqwest::Error> {
+    let client = reqwest::Client::new();
+    let mut headers = HeaderMap::new();
+    headers.insert("token", "".parse().unwrap());
+    let url = format!("{}{}{}", "", pin, "");
+    Ok(client.get(url).headers(headers).send().await?.json::<HashMap<String, Value>>().await?)
+
+}
+
+async fn post() -> Result<HashMap<String, String>, reqwest::Error> {
+    let client = reqwest::Client::new();
+
+    let mut headers = HeaderMap::new();
+    headers.insert("Content-Type", "application/json".parse().unwrap());
+
+    let mut data = HashMap::new();
+    data.insert("user", "zhangsan");
+    data.insert("password", "");
+
+    Ok(client.post("https://httpbin.org/ip").headers(headers).json(&data).send().await?.json::<HashMap<String, String>>().await?)
+}
+
+#[tokio::main]
+async fn main() {
+    if let Ok(resp) = get("dzllikelsw").await {
+        println!("{:#?}", resp);
+    }
+
+    // if let Ok(resp) = post().await {
+    //     println!("{:#?}", resp);
+    // }
+}
