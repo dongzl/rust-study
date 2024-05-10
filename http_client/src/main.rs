90156885af42a4f7d266e0d3dba99e36ff73e921
Zonglei Dong <dongzonglei@apache.org>
2024-05-10 09:30:52 +0800

    rust study

--- http_client/src/main.rs
+++ http_client/src/main.rs
@@ -1,14 +1,19 @@
-use std::collections::HashMap;
 use reqwest::header::HeaderMap;
 use serde_json::Value;
+use std::collections::HashMap;
 
 async fn get(pin: &str) -> Result<HashMap<String, Value>, reqwest::Error> {
     let client = reqwest::Client::new();
     let mut headers = HeaderMap::new();
     headers.insert("token", "".parse().unwrap());
     let url = format!("{}{}{}", "", pin, "");
-    Ok(client.get(url).headers(headers).send().await?.json::<HashMap<String, Value>>().await?)
-
+    Ok(client
+        .get(url)
+        .headers(headers)
+        .send()
+        .await?
+        .json::<HashMap<String, Value>>()
+        .await?)
 }
 
 async fn post() -> Result<HashMap<String, String>, reqwest::Error> {
@@ -21,7 +26,14 @@ async fn post() -> Result<HashMap<String, String>, reqwest::Error> {
     data.insert("user", "zhangsan");
     data.insert("password", "");
 
-    Ok(client.post("https://httpbin.org/ip").headers(headers).json(&data).send().await?.json::<HashMap<String, String>>().await?)
+    Ok(client
+        .post("https://httpbin.org/ip")
+        .headers(headers)
+        .json(&data)
+        .send()
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?)
 }
 
 #[tokio::main]
